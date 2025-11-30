f0473fb7cb4a51dff02e8883e7dbd81ca33f0ee9
oleksandradanyliuk <sashadanyliuk33@gmail.com>
2025-11-30 14:54:05 +0200

    Main.java

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -7,14 +7,14 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        // Example 1: l1 = [1,2,4], l2 = [1,3,4]
+      
         ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
         ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
         ListNode merged1 = solution.mergeTwoLists(l1, l2);
         System.out.print("Example 1: ");
         merged1.printList();
 
-        // Example 2: l1 = [], l2 = []
+  
         ListNode l3 = null;
         ListNode l4 = null;
         ListNode merged2 = solution.mergeTwoLists(l3, l4);
@@ -22,15 +22,14 @@ public class Main {
         if (merged2 != null) merged2.printList();
         else System.out.println("[]");
 
-        // Example 3: l1 = [], l2 = [0]
+    
         ListNode l5 = null;
         ListNode l6 = new ListNode(0);
         ListNode merged3 = solution.mergeTwoLists(l5, l6);
         System.out.print("Example 3: ");
         merged3.printList();
 
-        // ----------------------
-        // Використання LinkedList та Collections
+        
         LinkedList<Integer> list1 = new LinkedList<>();
         LinkedList<Integer> list2 = new LinkedList<>();
         Collections.addAll(list1, 1, 2, 4);
