3d13a4cb32708d70cf1438a7c245320b2d151d1d
oleksandradanyliuk <sashadanyliuk33@gmail.com>
2025-11-30 14:53:36 +0200

    ListNode.java

--- src/LinkedList/ListNode.java
+++ src/LinkedList/ListNode.java
@@ -8,7 +8,6 @@ public class ListNode {
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
-    // Метод для друку списку
     public void printList() {
         ListNode current = this;
         while (current != null) {
